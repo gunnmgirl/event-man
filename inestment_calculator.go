2c51e5b066a5998fd7d172063af9fe5434226adc
gunnmgirl <gunnmgirl@gmail.com>
2024-09-04 23:40:42 +0200

    feat: allow user to enter variable values

--- inestment_calculator.go
+++ inestment_calculator.go
@@ -6,9 +6,19 @@ import (
 )
 
 func main() {
-	investmentAmount := 1000
-	expectedReturnRate := 5.5
-	years := 15
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years int
+
+	// Prompt the user for input
+	fmt.Print("Enter the investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Enter the expected return rate (in %): ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Enter the number of years: ")
+	fmt.Scan(&years)
 
 	futureValue := float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
 	fmt.Println(futureValue)
