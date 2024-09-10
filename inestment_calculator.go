98cc3a7f0a95514485bd03b4e14094a1470de182
gunnmgirl <gunnmgirl@gmail.com>
2024-09-10 22:31:44 +0200

    feat: adds profit calculator programme

--- inestment_calculator.go
+++ inestment_calculator.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 // Format as currency
@@ -10,45 +9,31 @@ func formatCurrency(amount float64) string {
 	return fmt.Sprintf("$%.2f", amount)
 }
 
-func calculateFutureValue(investmentAmount, expectedReturnRate float64, years int, annualContribution float64) float64 {
-	futureValue := investmentAmount
-	for i := 1; i <= years; i++ {
-		// Calculate interest
-		futureValue += futureValue * (expectedReturnRate / 100)
-		// Add annual contributions
-		futureValue += annualContribution
-		fmt.Printf("Year %d: %s\n", i, formatCurrency(futureValue))
-	}
-	return futureValue
+func calculateEarnings(revenueVal, ExpensesVal, taxRateVal float64) (float64, float64, float64) {
+	earningsBeforeTax := revenueVal - ExpensesVal
+	taxAmount := earningsBeforeTax * (taxRateVal / 100)
+	earningsAfterTax := earningsBeforeTax - taxAmount
+	ratio := earningsBeforeTax / earningsAfterTax
+
+	return earningsBeforeTax, earningsAfterTax, ratio
 }
 
 func main() {
-	const inflationRate = 2.5
-	var investmentAmount, expectedReturnRate, annualContribution float64
-	var years int
+	const taxRate = 19 // in percentage
+	var revenue, expenses float64
 
-	fmt.Println("Answer the questions and get your future value calculated with yearly breakdown!")
+	fmt.Println("Answer the questions and get value of your earnings before and after taxes")
 
-	// Prompt the user for input
-	fmt.Print("Enter the initial investment amount: ")
-	fmt.Scan(&investmentAmount)
+	fmt.Print("Enter the revenue: ")
+	fmt.Scan(&revenue)
 
-	fmt.Print("Enter the expected annual return rate (in %): ")
-	fmt.Scan(&expectedReturnRate)
+	fmt.Print("Enter the expected expenses: ")
+	fmt.Scan(&expenses)
 
-	fmt.Print("Enter the number of years: ")
-	fmt.Scan(&years)
+	// Calculate earnings
+	ebt, profit, ratio := calculateEarnings(revenue, expenses, taxRate)
 
-	fmt.Print("Enter the annual contribution amount: ")
-	fmt.Scan(&annualContribution)
-
-	// Calculate future value
-	futureValue := calculateFutureValue(investmentAmount, expectedReturnRate, years, annualContribution)
-
-	futureRealValue := futureValue / math.Pow(1+inflationRate/100, float64(years))
-
-	// Display final value
-	fmt.Println("\nFinal Future Value:")
-	fmt.Println(formatCurrency(futureValue))
-	fmt.Println("Future real value: ", futureRealValue)
+	fmt.Println("Earnings before tax are: ", ebt)
+	fmt.Println("Earnings after tax are: ", profit)
+	fmt.Println("Ratio EBT/profit is: ", ratio)
 }
