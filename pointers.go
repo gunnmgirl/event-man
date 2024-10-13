0841b1bac404e4c570768efe2077b1fcbe0b4104
gunnmgirl <gunnmgirl@gmail.com>
2024-10-13 21:51:39 +0200

    feat: get adult years using pointer

--- pointers.go
+++ pointers.go
@@ -5,10 +5,12 @@ import "fmt"
 func main() {
 	age := 32
 
+	agePointer := &age
+
 	fmt.Println("Age: ", age)
-	fmt.Println("Adult years: ", getAdultYears(age))
+	fmt.Println("Adult years: ", getAdultYears(agePointer))
 }
 
-func getAdultYears(age int) int {
-	return age - 18
+func getAdultYears(age *int) int {
+	return *age - 18
 }
