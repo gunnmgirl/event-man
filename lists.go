f14946ece050d7f631513ff571114a5815b00c52
gunnmgirl <gunnmgirl@gmail.com>
2024-11-25 22:13:15 +0100

    feat: creates slice from price array

--- lists.go
+++ lists.go
@@ -5,5 +5,7 @@ import "fmt"
 func main() {
 	prices := [4]float64{10.99, 9.99, 45.99, 20.00}
 	fmt.Println("prices-", prices[3])
-	return
+
+	featuredPrices := prices[1:3] // second value is excluded
+	fmt.Println("featuredPrices-", featuredPrices)
 }
