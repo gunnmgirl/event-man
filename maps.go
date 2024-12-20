0650e31040abcefab410cea8faefb1c9e0a6f2bf
gunnmgirl <gunnmgirl@gmail.com>
2024-12-20 23:25:30 +0100

    feat: creates websites map

---
+++ maps.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	websites := map[string]string{
+		"Google":              "google.com",
+		"Amazon Web Services": "aws.com",
+	}
+	fmt.Println("websites-", websites)
+}
