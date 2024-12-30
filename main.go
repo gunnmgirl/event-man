98162b53da2c6cd9d79225c95fa42e33af022665
gunnmgirl <gunnmgirl@gmail.com>
2024-12-30 12:03:55 +0100

    feat: passing function as value

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import "fmt"
 func main() {
 	numbers := []int{1, 2, 3, 4}
 
-	doubles := doubleNumbers(&numbers)
+	doubles := transformNumbers(&numbers, double)
 	fmt.Println("doubles-", doubles)
+	triples := transformNumbers(&numbers, triple)
+	fmt.Println("triples-", triples)
 }
 
-func doubleNumbers(numbers *[]int) []int {
+func transformNumbers(numbers *[]int, transformFunc func(int) int) []int {
 	dNumbers := []int{}
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, val*2)
+		dNumbers = append(dNumbers, transformFunc(val))
 	}
 	return dNumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
 }
