8959ac1b358f33f1730deb8a92ce2c7b39ff65c8
gunnmgirl <gunnmgirl@gmail.com>
2024-12-31 16:20:42 +0100

    feat: return function from a function

--- main.go
+++ main.go
@@ -7,9 +7,9 @@ type transformFn func(int) int
 func main() {
 	numbers := []int{1, 2, 3, 4}
 
-	doubles := transformNumbers(&numbers, double)
+	doubles := transformNumbers(&numbers, getTransformerFunction(&numbers))
 	fmt.Println("doubles-", doubles)
-	triples := transformNumbers(&numbers, triple)
+	triples := transformNumbers(&numbers, getTransformerFunction(&numbers))
 	fmt.Println("triples-", triples)
 }
 
@@ -27,4 +27,11 @@ func double(number int) int {
 
 func triple(number int) int {
 	return number * 3
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	}
+	return triple
 }
