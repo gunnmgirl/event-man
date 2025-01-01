66e10bafded6fa4b8da51b06dd54ca151ba74a5c
gunnmgirl <gunnmgirl@gmail.com>
2025-01-01 23:51:04 +0100

    feat: declares anonymous funtion

--- main.go
+++ main.go
@@ -2,36 +2,22 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
+	numbers := []int{1, 2, 3}
 
-	doubles := transformNumbers(&numbers, getTransformerFunction(&numbers))
-	fmt.Println("doubles-", doubles)
-	triples := transformNumbers(&numbers, getTransformerFunction(&numbers))
-	fmt.Println("triples-", triples)
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
+
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transformFunc transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
-		dNumbers = append(dNumbers, transformFunc(val))
+		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
-}
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	}
-	return triple
 }
