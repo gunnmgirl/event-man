341f36e2f6a46cbaae670b8aef204e80f29f10bd
gunnmgirl <gunnmgirl@gmail.com>
2025-01-03 00:04:12 +0100

    feat: adds starting num for sumup

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(sumup(5, 5, 5))
+	fmt.Println(sumup(1, 5, 5, 5))
 }
 
-func sumup(numbers ...int) int {
-	sum := 0
+func sumup(startingNum int, numbers ...int) int {
+	sum := startingNum
 
 	for _, val := range numbers {
 		sum = sum + val
