a42dedeb63a3d5507861b42f628f3df47e6c1456
gunnmgirl <gunnmgirl@gmail.com>
2025-01-02 19:04:08 +0100

    feat: using recursion to find number factorial

--- main.go
+++ main.go
@@ -7,9 +7,14 @@ func main() {
 }
 
 func factorial(number int) int {
-	result := 1
-	for i := 1; i <= number; i++ {
-		result = result * i
+	if number == 0 {
+		return 1
 	}
-	return result
+	return number * factorial((number - 1))
+
+	// result := 1
+	// for i := 1; i <= number; i++ {
+	// 	result = result * i
+	// }
+	// return result
 }
